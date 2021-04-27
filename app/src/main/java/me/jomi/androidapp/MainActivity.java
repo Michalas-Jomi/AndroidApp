4dd6e5537f9f12e49020405940b4d95a7b9ff7d9
koral <orszulak.k@o2.pl>
2021-04-27 16:46:44 +0200

    google dependency, AuthStateListener

--- app/src/main/java/me/jomi/androidapp/MainActivity.java
+++ app/src/main/java/me/jomi/androidapp/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import me.jomi.androidapp.api.Api;
 import me.jomi.androidapp.listeners.AuthStateListener;
 
@@ -30,5 +33,18 @@ public class MainActivity extends AppCompatActivity  {
         super.onStart();
         instance = this;
         Api.auth.addAuthStateListener(new AuthStateListener());
+
+        Api.database.getReference().child("Users").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    for(DataSnapshot data : task.getResult().getChildren()) {
+                        System.out.println(data.getKey());
+                    }
+
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/me/jomi/androidapp/listeners/AuthStateListener.java
+++ app/src/main/java/me/jomi/androidapp/listeners/AuthStateListener.java
@@ -9,11 +9,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import me.jomi.androidapp.LoginActivity;
 import me.jomi.androidapp.MainActivity;
 import me.jomi.androidapp.UserProfile;
 import me.jomi.androidapp.api.Api;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AuthStateListener implements FirebaseAuth.AuthStateListener {
     @Override
@@ -27,10 +31,32 @@ public class AuthStateListener implements FirebaseAuth.AuthStateListener {
 
         if (user != null && user.isEmailVerified()) {
             MainActivity.instance.startActivity(new Intent(MainActivity.instance, UserProfile.class));
+            MainActivity.instance.finish(); // niszczy instancje, nie mozna juz do niej wrocic.
+
+            Api.database.getReference().child("Users").child(Api.auth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(Task<DataSnapshot> task) {
+                    if(task.isSuccessful()){
+                        if(task.getResult().exists()) System.out.println("istnieje w bazie - nie tworzymy uzytkownika");
+                        else System.out.println("nie istnieje w bazie - tworzymy dane usera");
+                    }
+                }
+            });
         }
         else
             MainActivity.instance.startActivity(new Intent(MainActivity.instance, LoginActivity.class));
 
 
+    }
+
+    private void greetings(){
+        Api.database.getReference().child("Users").child(Api.auth.getCurrentUser().getUid()).child("name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(Task<DataSnapshot> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.instance, "Hej " + task.getResult().getValue(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
